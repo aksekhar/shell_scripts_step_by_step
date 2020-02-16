43996faa66fa32ad217cb359ff2d96777c46d74c
akshyan <aksbarik.learning@gmail.com>
2020-02-16 09:26:15 +0530

    basic scripts

---
+++ bash_scripting.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#Above line point respective shell
+a=10
+(a=20)
+ echo "A VALUE NOT CHANGED: "$a
+ #Below lins use change the value of variable
+{
+	a=20
+ }
+echo "A VALUE   CHANGED: "$a
+b="setting b value"
+ echo $b
+
+ x=121
+
+echo -n This bash shell script "\n \n" writing using "\t"mac os
+
+echo -e This bash shell script "\n \n" writing using "\t"mac os
